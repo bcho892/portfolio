4f18cfeffa2a9d139c6bd2f22dda6bd35384c5b4
bcho892 <bcho892@aucklanduni.ac.nz>
2022-07-01 21:04:08 +1200

    added relevant information to text fields

--- home.js
+++ home.js
@@ -7,6 +7,11 @@ const projectsSection = document.getElementById("projectstrail");
 const contactSection = document.getElementById("contacttrail");
 const mobileMenu = document.getElementById("actions");
 const menuIcon = document.getElementById("hamburger");
+const loadingBar = document.getElementById("progressbar");
+
+const getScrollPercentage = () => {
+    return window.scrollY / (document.body.offsetHeight - window.innerHeight);   
+} // adapted from https://stackoverflow.com/questions/51364669/get-scroll-percentage-between-0-and-1-when-scrolling
 
 const isInView = (element) => {
     return element.getBoundingClientRect().top < window.innerHeight && element.getBoundingClientRect().bottom >= 0;
@@ -21,9 +26,15 @@ const toggleMenu = () => {
     menuIcon.classList.toggle("menuopen");
 }
 
+const openLink = (url) => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+}
+
 document.addEventListener('scroll', () => {
     growTrail(aboutSection);
     growTrail(skillsSection)
     growTrail(projectsSection);
     growTrail(contactSection);
+    loadingBar.style.width = 100 * getScrollPercentage() + "%";
 })
+
